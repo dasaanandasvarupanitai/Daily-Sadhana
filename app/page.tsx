4cc8e0758217f2cc9b971901dccfccdabe94287a
Anandasvarupa Nitai dasa <dasa.anandasvarupanitai@gmail.com>
2026-03-11 00:14:39 +0600

    previous listenings will be shown to logged-in users only

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
               alreadySubmitted={submittedToday}
             />
 
-            <PreviousListeningsList />
+            {user && <PreviousListeningsList />}
           </>
         )}
 
